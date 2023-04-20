308de73e8bd51c8532f42873963fbd62a885435d
VSapiak <vsapiak5@gmail.com>
2023-04-20 14:26:31 +0100

    First home work
    random color time style.body

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+	return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+ }
+
+btnStart.addEventListener('click', function() {
+		timeId = setInterval(() => {
+			document.body.style.backgroundColor = getRandomHexColor();
+		}, 1000);
+		btnStart.disabled = true;
+});
+
+btnStop.addEventListener('click', function() {
+	const timeId = null;
+	clearInterval(timeId);
+	btnStart.disabled = false;
+});
