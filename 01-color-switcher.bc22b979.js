e991c16c41114282e78b4b5bea68c94884ecede4
VSapiak <117464226+VSapiak@users.noreply.github.com>
2023-04-22 08:19:33 +0000

    Deploying to gh-pages from @ VSapiak/goit-js-hw-09@503d445d02b41d407732984e6466122d35fd9242 🚀

---
+++ 01-color-switcher.bc22b979.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),a=document.body.style;let d=null;t.addEventListener("click",(()=>{d=setInterval((()=>{a.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.disabled=!0})),e.addEventListener("click",(()=>{clearInterval(d),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.bc22b979.js.map
