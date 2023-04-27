762552a2d5f0b18c5cb550346cecf9bfa8612727
VSapiak <117464226+VSapiak@users.noreply.github.com>
2023-04-27 10:56:53 +0000

    Deploying to gh-pages from @ VSapiak/goit-js-hw-09@d4fa100172431d05856b05bad3674f619b8f9edf 🚀

---
+++ 01-color-switcher.9c23caff.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),e=document.body.style;var a=null;t.addEventListener("click",(function(){a=setInterval((function(){e.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),500),t.disabled=!0})),n.addEventListener("click",(function(){clearInterval(a),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.9c23caff.js.map
--- 01-color-switcher.e0878fab.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),e=document.body.style;var a=null;t.addEventListener("click",(function(){a=setInterval((function(){e.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.disabled=!0})),n.addEventListener("click",(function(){clearInterval(a),t.disabled=!1}))}();
-//# sourceMappingURL=01-color-switcher.e0878fab.js.map
